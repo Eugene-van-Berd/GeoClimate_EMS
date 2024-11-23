a1aee3e488fc2102473713bf6b75cbc316dc60b5
Eugene_van_Berd <eugenevanberd@gmail.com>
2024-11-23 01:47:13 +0100

    fix: download geomagnetic indices, update import and tidy scripts

--- scripts/Import.R
+++ scripts/Import.R
@@ -3,25 +3,14 @@ library(readxl)
 list.files("data/raw")
 data_raw <- read_excel("data/raw/database_2024-11-09.xlsx")
 
-# Загрузка индексов геомагнитной активности из GFZ_Potsdam
+# Загрузка индексов геомагнитной активности из GFZ, Potsdam
 library(httr)
 library(jsonlite)
-library(tidyverse)
+response_Ap <- GET("https://kp.gfz-potsdam.de/app/json/?start=2006-01-01T00:00:00Z&end=2022-12-31T23:59:59Z&index=Ap")
+GFZ_Potsdam_Ap_raw <- fromJSON(content(response_Ap, "text", encoding = "UTF-8"))
 
-## API-запросы
-response_Kp <- GET("https://kp.gfz-potsdam.de/app/json/?start=2006-01-01T00:00:00Z&end=2024-10-31T23:59:59Z&index=Kp")
-response_ap <- GET("https://kp.gfz-potsdam.de/app/json/?start=2006-01-01T00:00:00Z&end=2024-10-31T23:59:59Z&index=ap")
-response_Ap <- GET("https://kp.gfz-potsdam.de/app/json/?start=2006-01-01T00:00:00Z&end=2024-10-31T23:59:59Z&index=Ap")
-
-## Парсинг JSON
-GFZ_Potsdam_Kp <- fromJSON(content(response_Kp, "text", encoding = "UTF-8"))
-GFZ_Potsdam_ap <- fromJSON(content(response_ap, "text", encoding = "UTF-8"))
-GFZ_Potsdam_Ap <- fromJSON(content(response_Ap, "text", encoding = "UTF-8"))
-
-## Сборка данных
-tibble(datetime = GFZ_Potsdam_Kp$datetime, 
-       Kp = GFZ_Potsdam_Kp$Kp, 
-       datetime_ap = GFZ_Potsdam_ap$datetime, 
-       ap = GFZ_Potsdam_ap$ap
-       )
-
+# Загрузка индексов геомагнитной активности из WDC for Geomagnetism, Kyoto
+WDC_Kyoto_Dst_raw <- read_fwf("data/raw/WDC_Kyoto.dat", 
+                          fwf_widths( c(3, 2, 2, 1, 2, 2, 1, 1, 2, 4, rep(4, 24), 4) )
+                          )
+          
--- scripts/Tidy_Data.R
+++ scripts/Tidy_Data.R
@@ -2,4 +2,50 @@
 library(tidyverse)
 library(skimr)
 
+#Индекс Ap за 2006-2022гг
+GFZ_Potsdam_Ap <- tibble(Date = as.Date(ymd_hms(GFZ_Potsdam_Ap_raw$datetime)),
+                         Ap_day_mean = GFZ_Potsdam_Ap_raw$Ap)
 
+#Индекс Dst за 2006-2022гг
+WDC_Kyoto_Dst <- WDC_Kyoto_Dst_raw %>% 
+  transmute(
+    Date = as.Date(paste0(X9,X2,"-",X3,"-",X5)), 
+    Dst_day_mean = X35
+  ) 
+
+#Данные для исследования
+skim(data_raw)
+
+data <- data_raw %>% 
+  transmute(
+    Date = dmy(Дата), EMS = `Вызов СМП с диагнозом ОНМК у всех пациентов старше 25 лет`, 
+    Sunspot_n = `Международное число солнечных пятен`, F10.7 = `Cкорректированный (F10.7)`, 
+    Temp_min = `Минимальная температура`, Temp_max = `Максимальная температура`, 
+    Temp_mean = `Средняя температура`, Temp_var = Temp_max - Temp_min,
+    Wind = `Скорость ветра`, Precipitation = `Количество осадков`,
+    Pressure_8 = `Атмосферное давление на уровне станции в 8:00`, 
+    Pressure_17 = `Атмосферное давление на уровне станции в 17:00`, 
+    Pressure_mean = `Атмосферное давление в среднем за сутки, С (Т)`, 
+    
+    #Индексы геомагнитной активности берем из открытых источников
+    #Много пропусков: Количество умерших, Магнитная буря
+    #Разбивку по полу и возрастной группе рассмотрим позже:
+    
+    #EMS_male_1 = `Количество вызовов СМП с диагнозом ОНМК у мужчин 25-44 лет`, 
+    #EMS_male_2 = `Количество вызовов СМП с диагнозом ОНМК у мужчин 45-54 лет`, 
+    #EMS_male_3 = `Количество вызовов СМП с диагнозом ОНМК у мужчин старше 55 лет`, 
+    #EMS_female_1 = `Количество вызовов СМП с диагнозом ОНМК у женщин 25-44 лет`, 
+    #EMS_female_2 = `Количество вызовов СМП с диагнозом ОНМК у женщин 45-54 лет`, 
+    #EMS_female_3 = `Количество вызовов СМП с диагнозом ОНМК у женщин старше 55 лет`, 
+    
+  ) %>% 
+  mutate(across(starts_with("Pressure"), ~ if_else(. < 700, NA, .))) %>% 
+  filter(!is.na(Date)) %>% 
+  left_join(GFZ_Potsdam_Ap, by = join_by(Date)) %>% 
+  left_join(WDC_Kyoto_Dst, by = join_by(Date))
+  
+skim(data)
+data %>% head()
+  
+
+  
