8025076c930b0e5066ce228730340e17ee3aa7f9
Eugene_van_Berd <eugenevanberd@gmail.com>
2025-01-22 21:04:41 +0200

    docs: add K and Kp indices

--- scripts/Download.R
+++ scripts/Download.R
@@ -9,5 +9,17 @@ GFZ_Potsdam_Ap_raw <- fromJSON(content(response_Ap, "text", encoding = "UTF-8"))
 GFZ_Potsdam_Ap <- tibble(Date = as.Date(ymd_hms(GFZ_Potsdam_Ap_raw$datetime)),
                          Ap_mean = GFZ_Potsdam_Ap_raw$Ap)
 
+response_Kp <- GET("https://kp.gfz-potsdam.de/app/json/?start=2006-01-01T00:00:00Z&end=2022-12-31T23:59:59Z&index=Kp")
+GFZ_Potsdam_Kp_raw <- fromJSON(content(response_Kp, "text", encoding = "UTF-8"))
+GFZ_Potsdam_Kp <- tibble(Date = as.Date(ymd_hms(GFZ_Potsdam_Kp_raw$datetime)),
+       Point = rep(c("K_00", "K_03", "K_06", "K_09", "K_12", "K_15", "K_18", "K_21"), 6209),
+       Kp = GFZ_Potsdam_Kp_raw$Kp) %>% 
+  pivot_wider(names_from = Point, values_from = Kp) %>% 
+  rowwise() %>% 
+  mutate(across(starts_with("K"), ~ifelse(. == -1, NA, .)),
+         Kp_Sum = sum(c_across(starts_with("K")), na.rm = FALSE)
+  )
+
 ##Локальное храненение данных 
 write_csv(GFZ_Potsdam_Ap, "data/raw/GFZ_Potsdam.csv") 
+write_csv(GFZ_Potsdam_Kp, "data/raw/GFZ_Potsdam_Kp.csv") 
