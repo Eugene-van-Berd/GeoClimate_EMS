d2608c8e7e0b853ae106d9295adbc92bb6263525
Eugene_van_Berd <eugenevanberd@gmail.com>
2025-01-27 01:22:33 +0200

    upd: translate Download.R

--- scripts/Download.R
+++ scripts/Download.R
@@ -1,11 +1,11 @@
-#Eugene-van-Berd: Выгрузка геомагнитных индексов из GFZ, Potsdam 
+# API Data Extraction Script 
 
+## Packages
 library(tidyverse)
 library(httr)
 library(jsonlite)
 
-
-#Геомагнитных индексы
+## Geomagnetic indices (GFZ, Potsdam)
 response_Ap <- GET("https://kp.gfz-potsdam.de/app/json/?start=2006-01-01T00:00:00Z&end=2022-12-31T23:59:59Z&index=Ap")
 GFZ_Potsdam_Ap_raw <- fromJSON(content(response_Ap, "text", encoding = "UTF-8"))
 GFZ_Potsdam_Ap <- tibble(Date = as.Date(ymd_hms(GFZ_Potsdam_Ap_raw$datetime)),
@@ -23,34 +23,26 @@ GFZ_Potsdam_Kp <- tibble(Date = as.Date(ymd_hms(GFZ_Potsdam_Kp_raw$datetime)),
   )
 
 
-#Календарь
-pb <- progress_bar$new(
-  format = "  Processing [:bar] :percent in :elapsedfull",
-  total = length( seq.Date(from = as.Date("2006-01-01"),
-                           to = as.Date("2022-12-31"),
-                           by = "day")), clear = FALSE, width= 100)
+## Calendar (isDayOff)
+DayOff <- c()
+for (i in 2006:2022){
+ 
+  response <- GET(paste0("https://isdayoff.ru/api/getdata?year=",i)) %>% 
+                content("text", encoding = "UTF-8") %>% strsplit("") %>% unlist()
+            
+  DayOff <- c(DayOff, as.logical(as.numeric(response)))
 
-calendar <- tibble(
-  Date = c(), 
-  WorkDay = c()
-)
-
-for (i in seq.Date(from = as.Date("2006-01-01"),
-                   to = as.Date("2022-12-31"),
-                   by = "day")) {
-  
-  pb$tick()
-  Sys.sleep(1 / 100)
-  
-  calendar <- calendar %>% bind_rows(
-    tibble(Date = as.Date(i), 
-           WorkDay = GET(paste0("https://isdayoff.ru/", as.Date(i),"?cc=ru")) %>% 
-             content("text", encoding = "UTF-8") %>% fromJSON()
-    )
-  )
 }
 
+calendar <- tibble(
+  Date = seq.Date(from = as.Date("2006-01-01"), to = as.Date("2022-12-31"), by = "day"), 
+  Year = year(Date), Season = case_when(month(Date) %in% c(1, 2, 12) ~ "Winter", 
+                                        month(Date) %in% c(3, 4, 5) ~ "Spring",
+                                        month(Date) %in% c(6, 7, 8) ~ "Summer",
+                                        month(Date) %in% c(9, 10, 11) ~ "Autumn" ), 
+  Month = month(Date, label = TRUE, abbr = FALSE,  locale = "ENG"), DayOff = DayOff )
 
-##Локальное храненение данных 
+## Local storage
 write_csv(GFZ_Potsdam_Ap, "data/raw/GFZ_Potsdam.csv") 
 write_csv(GFZ_Potsdam_Kp, "data/raw/GFZ_Potsdam_Kp.csv") 
+write_csv(calendar, "data/raw/Calendar.csv") 
